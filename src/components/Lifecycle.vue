d63d13f8c97c9b6f0d86e1e0315602e6f7739432
SHINHYE <kshinhye1102@gmail.com>
2023-03-23 01:14:56 +0900

    lifecycle 설명 추가(확인필요)

--- src/components/Lifecycle.vue
+++ src/components/Lifecycle.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import axios from 'axios';
-import { onActivated, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onDeactivated, onErrorCaptured, onMounted, onRenderTracked, onRenderTriggered, onServerPrefetch, onUnmounted, onUpdated } from 'vue';
+import { onActivated, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onDeactivated, onErrorCaptured, onMounted, onRenderTracked, onRenderTriggered, onServerPrefetch, onUnmounted, onUpdated, ref } from 'vue';
 
 type Lifecycle = {
   optApi: string,
@@ -15,42 +15,42 @@ const lifecycleList :Lifecycle[] = [
     optApi: 'beforeCreated',
     compApi: 'X',
     desc: '<strong>Vue 인스턴스가 만들어지기 전에 호출</strong> <br><br>- 외부에서 들어오는 데이터인 props에 접근 가능 <br>- data, methods 등이 만들어지지 않아 this로 접근해서 컴포넌트 데이터와 관련된 작업할 수 X',
-    warn: '',
+    warn: 'setup에서 실행',
     ex:''
   },
   {
     optApi: 'created',
     compApi: 'X',
     desc: '<strong>Vue 인스턴스가 생성된 후, 호출</strong> <br><br>- data, computed, methods, watch 설정이 완료되어 this로 접근 가능 <br>',
-    warn: '',
+    warn: 'setup에서 실행',
     ex:''
   },
   {
     optApi: 'beforeMount',
     compApi: 'onBeforeMount',
-    desc: '<strong>Vue 인스턴스가 생성된 후, template옵션이 있으면 이를 컴파일하는데, 컴파일된 이후 호출됨</strong> <br><br>- 컴파일 된 이후에 특별히 할만한 일 X <br>- 이후 자식 컴포넌트가 생성되고 마운트',
-    warn: '',
-    ex:'컴포넌트가 마운트되기 전, 초기화 작업 수행시 사용'
+    desc: '<strong>Vue 인스턴스가 생성된 후, 컴포넌트가 DOM에 마운트 되기 전에 실행</strong> <br><br>- 렌더링 직전에 DOM을 변경하고자 할 떄 사용 ',
+    warn: '- 컴포넌트가 DOM에 마운트되기 전, 초기화 작업 수행시 사용 <br>- 외부 API와 통신하여 데이터를 가져올 떄 사용 <br>- Vuex 스토어 초기화, 이벤트 리스너 등록, 다른 컴포넌트와의 상호작용 등의 작업 수행 가능 <br>- DOM 요소를 직접 조작하는 것은 onMounted에서 수행하기(여기서 조작하면 레더링 지연 및 예기치 못한 상황 발생)',
+    ex:' <br>ex) Todo List를 불러오는 API호출로 미리 데이터 셋팅하면 더 빠르게 볼 수 있게 됨 <br><br>'
   },
   {
     optApi: 'mounted',
     compApi: 'onMounted',
     desc: '<strong>컴포넌트가 마운트된 이후 호출</strong> <br><br>- DOM 트리가 완성되어 DOM에 접근 가능  <br>- 모든 자식 컴포넌트들도 마운트 완료 상태 <br>- 비동기(async) 컴포넌트/ Suspense태그가 들어간 컴포넌트를 제외하고는 모든 자식 컴포넌트들이 마운트되었을 때 호출됨',
-    warn: '',
+    warn: '- 특정 DOM 요소를 참조하여 초기화 <br>- DOM 요소 선택 후, 이벤트 리스너 등록 <br>-외부 라이브러리를 초기화하는 등의 작업 수행',
     ex:''
   },
   {
     optApi: 'beforeUpdate',
     compApi: 'onBeforeUpdate',
-    desc: '<strong>컴포넌트 데이터가 변함으로 인해 DOM이 변화되기 전에 호출(데이터는 변했지만 아직 DOM변화 전)</strong> <br><br>- document.querySelector로 접근하면 이미 값이 변경됨<br>- 여기서 데이터 값을 변경시키면 그 값이 렌더링에 반영되고, 다시 이 훅이 실행되진 X',
-    warn: '- DOM 변경에 영향을 주는 데이터가 변하지 않으면 호출되지 X',
-    ex:''
+    desc: '<strong>컴포넌트 데이터가 변함으로 인해 DOM이 변화되기 전에 호출(데이터는 변했지만 아직 DOM변화 전)</strong> <br><br>- 컴포넌트가 재랜더링 되기 전에 실행되는 유일한 훅 <br>- 컴포넌트가 변경될 때마다 호출됨<br>- 여기서 데이터 값을 변경시키면 그 값이 렌더링에 반영되고, 다시 이 훅이 실행되진 X(DOM 변경에 영향을 주는 데이터가 변하지 않으면 호출되지 X )',
+    warn: '- 변경사항을 로깅하거나 비동기적으로 데이터를 가져오는 작업 수행',
+    ex:'<button @click="change">바꾸기</button>'
   },
   {
       optApi: 'updated',
       compApi: 'onUpdated',
       desc: '<strong>DOM이 변한 이후 호출</strong> <br><br>- 데이터 변경으로 가상 DOM이 다시 렌더링 되고 패치된 후에 호출됨. <br>- 이 훅이 호출된 시점은 컴포넌트의 DOM이 없데이트된 상태이므로 DOM에 종속적인 작업 가능',
-      warn: '- DOM 변경에 영향을 주는 데이터가 변하지 않으면 호출되지 X <br>-만약, 여기서 값을 변경한다면 다시 beforeUpdate 훅부터 호출 -> 무한루프',
+      warn: '- DOM 변경에 영향을 주는 데이터가 변하지 않으면 호출되지 X => 만약, 여기서 값을 변경한다면 다시 beforeUpdate 훅부터 호출 -> 무한루프 <br>- 컴포넌트 상태에 따라 화면에 표시되는 내용이 변경되는 경우' ,
       ex:''
     },
   {
@@ -73,6 +73,7 @@ const lifecycleList :Lifecycle[] = [
     desc: '<strong>자식 컴포넌트에서 에러가 발생했을 때 호출</strong> <br><br>- 자식 컴포넌트에서 발생한 에러가 부모 컴포넌트로 전파되는 것을 방지 <br>- 에러를 캐치해서 로그에 남기거나 특정 에러 메시지를 보여주는 등의 작업 수행 <br>- 부모 컴포넌트에서 처리하는 것이 일반적이나 에러가 난 자식 컴포넌트에서도 사용 가능 <br>-- 부모 컴포넌트에서 처리: 여러 자식 컴포넌트에서 발생한 에러를 한 번에 처리할 수 있어 유지보수 측면에서 좋고 더 효율적',
     warn: '<br>- false반환을 통해 더 이상 에러가 전파되지 않음',
     ex:'<button @click="errorFetchData">errorFetchData</button>'
+    // 여기서 버튼 눌렀을 때 동작을 어떻게 하나
   },
   {
     optApi: 'X',
@@ -118,6 +119,12 @@ const errorFetchData = async () => {
     console.error('Error caught in child component:', error);
     throw error; // 에러 전파 -> 전파를 해줘야 부모 컴포넌트에서 잡을 수 있음
   }
+}
+
+let value = ref('onBeforeUpdate');
+const change = () => {
+  value.value = 'called onBeforeUpdate'
+  console.log(value)
 }
 
 console.log('setup() => beforeCreated, created')
@@ -174,6 +181,8 @@ console.log('')
 </script>
 
 <template>
+  <button @click="change">바꾸기</button>
+  <p>{{ value }}</p>
   <button @click="errorFetchData">fetchData</button>
   <h1>Lifecycle</h1>
   <img src="https://vuejs.org/assets/lifecycle.16e4c08e.png" style="height: 50; width: 600px; ">
