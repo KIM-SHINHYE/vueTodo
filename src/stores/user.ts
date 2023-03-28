cac21c209bb32d086c3228f2ce8f8c7ccb20a957
KIM-SHINHYE <kshinhye1102@gmail.com>
2023-03-28 11:00:55 +0900

    store 저장 => 화면에 보여주는건 웬만하면 ref로 감싸기

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,15 +1,11 @@
 import { defineStore } from "pinia";
-import { computed } from "vue";
+import { computed, ref, type Ref } from "vue";
+
 
 type User = {
     email: string;
     first_name: string;
     last_name: string;
-
-    id: string;
-    name: string;
-    job: string;
-    createdAt: Date;
 }
 
 // type InputUser = {
@@ -21,28 +17,19 @@ type User = {
 
 // defineStore에 첫번째 파라미터에 고유이름, 두번째 파라미터에 다른 옵션 추가
 export const useUserStore = defineStore('userStore', () => {
-    let userList: User[] = []
-    let createList : User[] = [];
+    let userList: Ref<User[]> = ref([])
+    let createList :Ref<User[]> = ref([])
 
     const addCreateUser = (user: User) => {
-        createList.push(user)
+        createList.value.push(user)
     }
 
 
     const addUser = (user: User[]) => {
-        userList = [...createUsers.value,...user]
-        // console.log(users)
-        // users = [...createList]
-        // console.log(users)
+        userList.value = [...createList.value,...user]
     }
-    // const addUser2 = (user: InputUser) => {
-    //     users.push(user);
-    // }
-
-    // const addUsers = (userList: User[]) => {
-    //     users = userList;
-    // }
-    const createUsers = computed(() => createList);
-    const users = computed(() =>userList);
-    return {users,createUsers, addUser,addCreateUser}
+    
+    const createUsers = computed(() => createList.value);
+    const users = computed(() =>userList.value);
+    return {users,createUsers,addUser,addCreateUser}
 })
